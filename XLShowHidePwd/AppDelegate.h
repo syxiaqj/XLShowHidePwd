75046048e83b7796afaa10753843747586429983
员员 <syxiaqj@gmail.com>
2014-07-23 15:49:50 +0800

    Initial Commit

---
+++ XLShowHidePwd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLShowHidePwd
+//
+//  Created by xqj on 14-7-23.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XLShowHidePwd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XLShowHidePwd
+//
+//  Created by xqj on 14-7-23.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
